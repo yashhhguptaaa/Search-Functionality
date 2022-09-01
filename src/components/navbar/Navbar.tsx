a065d8ab39c7eec963c3f782896dd91af8df83bd
Yash Gupta <yash3144@gmail.com>
2022-09-02 01:52:58 +0530

    Changed border Color to blue, when div is in focus

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,14 +1,18 @@
+import { useState } from "react";
 import SmallcaseIcon from "../../assets/icons/Smallcase_Icon.svg";
 import SearchBox from "../searchbox/SearchBox";
 import style from "./Navbar.module.scss";
 
 export default function Navbar() {
+    let [showInfo1, setShowInfo1] = useState(false);
     return (
         <div className={`${style.Header_Container} ${style.Display_Flex}`}>
             <div className={`${style.Header_Container_Navbar_Wrapper} ${style.Display_Flex}`}>
                 <div className={`${style.Header_Container_Navbar} ${style.Display_Flex}`}>
                     <img src={SmallcaseIcon} style={{ height: "1.8rem", width: "6.9rem" }} alt="Smallcase Icon" />
-                    <SearchBox />
+                    <div onClick={() => setShowInfo1(true)}>
+                        <SearchBox show={showInfo1} onClickOutside={() => { setShowInfo1(false) }} />
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/searchbox/SearchBox.tsx
+++ src/components/searchbox/SearchBox.tsx
@@ -1,11 +1,29 @@
-import { useEffect, useState, useCallback } from "react";
+import { useEffect, useState, useCallback, useRef } from "react";
 import SearchIcon from "../../assets/icons/Search_Icon.svg";
 import style from "./SearchBox.module.scss";
 import axios from "axios";
 
-export default function SearchBox() {
+export default function SearchBox(props: any) {
+    const ref = useRef(null);
+    const { onClickOutside } = props;
+    console.log("props:", props)
 
     const [searchList, setSearchList] = useState([])
+    const [searchKeyword, setSearchKeyword] = useState("")
+
+    useEffect(() => {
+        const handleClickOutside = (event: any) => {
+            // @ts-ignore: Unreachable code error
+            if (ref.current && !ref.current.contains(event.target)) {
+                onClickOutside && onClickOutside();
+            }
+        };
+        document.addEventListener('click', handleClickOutside, true);
+        return () => {
+            document.removeEventListener('click', handleClickOutside, true);
+        };
+    }, [onClickOutside]);
+
 
     const getTickerFromAPi = async (word: string) => {
         console.log("word :", word)
@@ -32,16 +50,17 @@ export default function SearchBox() {
     const optimizedFn = useCallback(debounce(500), []);
 
     const handleSearch = (event: any) => {
-        if (event) {
+        if (event.target.value.length >= 3) {
             console.log("called");
+            setSearchKeyword(event.target.value);
             optimizedFn(event.target.value);
             // this.setState({ search: e }, () => getTickerFromAPi(event.target.value));
         }
     };
 
     return (
-        <div>
-            <div className={style.Search_Box_Wrapper}>
+        <div ref={ref}>
+            <div className={`${style.Search_Box_Wrapper} ${props.show ? style.Focused_Container : ""}`}>
                 <div className={`${style.Search_Box} ${style.Display_Flex}`}>
                     <img src={SearchIcon} className={style.Search_Icon_CSS} alt="Search Icon" />
                     <input
@@ -51,14 +70,14 @@ export default function SearchBox() {
                     />
                 </div>
             </div>
-            <div className={style.DropDown_List_Wrapper}>
+            {props.show && (<div className={style.DropDown_List_Wrapper}>
                 <div className={style.DropDown_List}>
                     {
                         searchList?.length
                             ?
-                            searchList.map((company: any) => {
+                            searchList.map((company: any, index: number) => {
                                 return (
-                                    <div className={style.DropDown_List_Content}>
+                                    <div key={index + company.symbol} className={style.DropDown_List_Content}>
                                         <div className={style.Content_Title}>
                                             <span className={style.First_Line_Title}>{company.symbol}</span>
                                             <p className={style.Second_Line_Title}>{company.name.length > 25 ? company.name.substring(0, 25) + "..." : company.name}</p>
@@ -67,17 +86,26 @@ export default function SearchBox() {
                                 )
                             })
                             :
-                            (
-                                <div className={style.DropDown_List_Content}>
-                                    <div className={style.Content_Title}>
-                                        <span className={style.First_Line_Title}>Type 3 or more characters</span>
-                                        <p className={style.Second_Line_Title}>to start searching</p>
+                            searchKeyword.length < 4
+                                ?
+                                (
+                                    <div className={style.DropDown_List_Content}>
+                                        <div className={style.Content_Title}>
+                                            <span className={style.First_Line_Title}>Type 3 or more characters</span>
+                                            <p className={style.Second_Line_Title}>to start searching</p>
+                                        </div>
                                     </div>
-                                </div>
-                            )
+                                ) : (
+                                    <div className={style.DropDown_List_Content}>
+                                        <div className={style.Content_Title}>
+                                            <span className={style.First_Line_Title}>No Company Exists</span>
+                                            <p className={style.Second_Line_Title}>with your search query</p>
+                                        </div>
+                                    </div>
+                                )
                     }
                 </div>
-            </div>
+            </div>)}
         </div>
     )
 }
